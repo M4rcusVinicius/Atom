dae2b463a03514d31795e021434b228f0df0d5c1
M4rcusVinicius <marcus.vinicius.mvap@gmail.com>
2021-05-18 15:56:58 -0300

    Display atom

--- components/Atom/index.js
+++ components/Atom/index.js
@@ -1,12 +1,10 @@
 import React from "react";
 import db from "./db";
 import { circuit } from "./process";
-import Electrons from "./Electrons";
 
 import * as S from "./styled";
 
 const Atom = ({ number }) => {
-  const [atom, setAtom] = React.useState(db[6]);
 
   const layers = [
     { size: 100 },
@@ -15,23 +13,38 @@ const Atom = ({ number }) => {
     { size: 250 },
     { size: 300 },
     { size: 350 },
-  ];
+    { size: 400 },
+  ]; 
+
+  const [atom, setAtom] = React.useState(() => {
+    const at = db[6]
+    at.electrons = at.layers.map((q, index) => circuit(q, layers[index].size / 2))
+    return at
+  });
 
   React.useEffect(() => {
-    const at = db[number]
-    at.electron = at.layers.map((q, index) => circuit(q, layers[index].size / 2))
-    console.log(at)
+    try {
+      const at = db[number]
+      at.electrons = at.layers.map((q, index) => circuit(q, layers[index].size / 2))
+      setAtom(at)
+    } catch(err) {
+      console.log("Can't precess atom with this number" , err)
+    }
   }, [number]);
 
   return (
     <S.Wrapper>
       <S.Center />
-      {/* {atom.layers.map((layer, index) =>  (
+      {atom.electrons.map((electrons, index) =>  {
+        console.log('Electrons:', electrons.map(pos => pos))
+        return (
         <>
           <S.Circuit size={layers[index].size} key={"Layer " + index} />
-          <Electrons electron={layer} />
+          {electrons.map((pos, i) => (
+            <S.Circle top={pos.top} left={pos.left} key={`Electron ${i} - (${pos.top}, ${pos.left})'`}/>
+          ))}
         </>
-      ))} */}
+      )})}
     </S.Wrapper>
   );
 };
