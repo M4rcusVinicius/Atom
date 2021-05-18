42e8c6293992ca31e440ae97f6c47d8008e1bcbb
M4rcusVinicius <marcus.vinicius.mvap@gmail.com>
2021-05-18 20:11:00 -0300

    Atom size

--- components/Atom/index.js
+++ components/Atom/index.js
@@ -1,10 +1,11 @@
 import React from "react"
 import * as S from "./styled"
 
-const Atom = () => (
+const Atom = () => {
+  return (
   <S.Wrapper>
     Atom
   </S.Wrapper>
-)
+)}
 
 export default Atom
--- components/Atom/styled.js
+++ components/Atom/styled.js
@@ -1,4 +1,7 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
+  background-color: black;
+  height: 100vh;
+  width: 100vh;
 `;
