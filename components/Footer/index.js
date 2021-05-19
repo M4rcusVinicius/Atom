c65ddfa2c4c3270022af405879988bec1b285589
M4rcusVinicius <marcus.vinicius.mvap@gmail.com>
2021-05-19 20:14:45 -0300

    Footer

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,8 +1,18 @@
 import React from "react"
+import Image from 'next/image'
 import * as S from "./styled"
 
 const Footer = () => (
   <S.Wrapper>
+    
+    <S.Image>
+      <Image
+        src="/Profile.png"
+        alt="Picture of the author"
+        width={80}
+        height={80}
+      />
+    </S.Image>
     <S.Title>Distribuição Eletrônica de Linus Pauling</S.Title>
   </S.Wrapper>
 )
