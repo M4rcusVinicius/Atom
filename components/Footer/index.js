50326503000542534aa883167604f767eed587ec
M4rcusVinicius <marcus.vinicius.mvap@gmail.com>
2021-05-19 16:32:57 -0300

    Footer

---
+++ components/Footer/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+import * as S from "./styled"
+
+const Footer = () => (
+  <S.Wrapper>
+    <S.Title>Distribuição Eletrônica de Linus Pauling</S.Title>
+  </S.Wrapper>
+)
+
+export default Footer
---
+++ components/Footer/styled.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  height: 5rem;
+  width: 100%;
+  padding: 0 2rem;
+  background-color: #161b22;
+  display: flex;
+  align-items: center;
+`;
+
+export const Title = styled.h1`
+  font-size: 1.2rem;
+  font-weight: 600;
+`;
