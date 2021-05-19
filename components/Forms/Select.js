d8e179b96106337ce341b72b20a1a52f68818292
M4rcusVinicius <marcus.vinicius.mvap@gmail.com>
2021-05-19 08:58:15 -0300

    Select

---
+++ components/Forms/Select.js
@@ -0,0 +1,11 @@
+import React from "react"
+import * as S from "./styled"
+
+const Select = () => {
+  return (
+  <S.Select>
+
+  </S.Select>
+)}
+
+export default Select
--- components/Forms/styled.js
+++ components/Forms/styled.js
@@ -33,3 +33,8 @@ export const InputText = styled.input`
   border-radius: 5px;
 `;
 
+
+export const Select = styled.div`
+
+`;
+
