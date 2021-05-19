eb70981bfd82d1c39c728ca022e99f172b8439e3
M4rcusVinicius <marcus.vinicius.mvap@gmail.com>
2021-05-19 08:09:01 -0300

    Play button

--- components/Atom/index.js
+++ components/Atom/index.js
@@ -1,38 +1,47 @@
-import React from "react"
-import * as S from "./styled"
+import React from "react";
+import * as S from "./styled";
+import * as I from "@styled-icons/boxicons-regular/";
 
 const Atom = ({ layers }) => {
+  const [play, setPlay] = React.useState(true);
+
   return (
     <S.Wrapper>
+      <S.Button
+        onClick={() => {
+          setPlay(!play);
+        }}
+      >
+        {play ? <I.Stop /> : <I.Play />}
+      </S.Button>
       <S.Core />
-      { layers && layers.map((amount, index) => {
-          const electronPosition = calcPosition(amount, index)
+      {layers &&
+        layers.map((amount, index) => {
+          const electronPosition = calcPosition(amount, index);
           return (
-            <S.Layer >
-              <S.Circuit index={(index + 1)} />
-              {
-                electronPosition.map(pos => (
-                  <S.Electron top={pos.top} left={pos.left} index={(index + 1)} />
-                ))
-              }
+            <S.Layer>
+              <S.Circuit index={index + 1} />
+              {electronPosition.map((pos) => (
+                <S.Electron top={pos.top} left={pos.left} index={index + 1} />
+              ))}
             </S.Layer>
-          )
-        })
-      }
+          );
+        })}
     </S.Wrapper>
-)}
+  );
+};
 
 const calcPosition = (amount) => {
   // Multiplicar pelo raio e subtrair pela metade da altura total menos a metade do átomo => res * size - ( width / 2 - rad / 2 )
-  const div = 360 / amount
-  const electrons = new Array
+  const div = 360 / amount;
+  const electrons = new Array();
   for (var i = 1; i <= amount; ++i) {
     electrons.push({
       top: Math.sin(div * i * (Math.PI / 180)),
-      left: Math.cos(div * i * (Math.PI / 180))
-    })
+      left: Math.cos(div * i * (Math.PI / 180)),
+    });
   }
-  return electrons
-}
+  return electrons;
+};
 
-export default Atom 
+export default Atom;
