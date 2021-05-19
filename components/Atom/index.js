41403496127717f8c0c4f9f1b6aa177c0c5305ce
M4rcusVinicius <marcus.vinicius.mvap@gmail.com>
2021-05-18 21:57:41 -0300

    Atom

--- components/Atom/index.js
+++ components/Atom/index.js
@@ -4,7 +4,7 @@ import * as S from "./styled"
 const Atom = () => {
   return (
   <S.Wrapper>
-    Atom
+    <S.Core />
   </S.Wrapper>
 )}
 
--- components/Atom/styled.js
+++ components/Atom/styled.js
@@ -4,4 +4,17 @@ export const Wrapper = styled.div`
   background-color: black;
   height: 100vh;
   width: 100vh;
+  position: relative;
+  overflow: hidden;
+`;
+
+export const Core = styled.div`
+  position: absolute;
+  border-radius: 50%;
+  width: 30px;
+  height: 30px;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #96bcff;
 `;
