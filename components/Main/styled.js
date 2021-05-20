1a09560123677c7e0910367ee2442d52add76075
M4rcusVinicius <marcus.vinicius.mvap@gmail.com>
2021-05-20 08:34:05 -0300

    Mobaile main

--- components/Main/styled.js
+++ components/Main/styled.js
@@ -4,4 +4,9 @@ export const Wrapper = styled.div`
   height: 100vh;
   width: calc(100% - 100vh);
   border-left: 2px solid #62656a;
+
+  @media(max-width: 450px) {
+    height: calc(100% - 100vw);
+    width: 100%;
+  }
 `;
