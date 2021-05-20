93faa2cabeb1308abb19506f347a88e34f5f63fb
M4rcusVinicius <marcus.vinicius.mvap@gmail.com>
2021-05-20 09:08:46 -0300

    Select mobile

--- components/Forms/styled.js
+++ components/Forms/styled.js
@@ -71,6 +71,11 @@ export const SelectWrapper = styled.div`
   width: 100%;
   padding: 0 2rem;
   display: ${props => props.select ? 'block' : 'none'};
+
+  @media(max-width: 450px) {
+    top: 8rem;
+    padding: 0 1rem;
+  }
 `;
 
 export const SelectContainer = styled.div`
@@ -79,16 +84,25 @@ export const SelectContainer = styled.div`
   background-color: #000;
   border-radius: 5px;
   height: 20rem;
+
+  @media(max-width: 450px) {
+    padding: 1rem;
+    height: calc(100vh - 100vw - 14rem);
+  }
 `;
 
 export const Select = styled.div`
   max-height: 17rem;
   width: 100%;
-  display: grid;
-  grid-gap: 1rem;
-  grid-template-columns: repeat(auto-fit,minmax(12rem,1fr));
+  display: flex;
+  flex-wrap: wrap;
   overflow: auto;
+  justify-content: space-between;
   scrollbar-width: none;
+
+  @media(max-width: 450px) {
+    height: calc(100vh - 100vw - 16rem);
+  }
 `;
 
 export const Item = styled.div`
@@ -106,12 +120,18 @@ export const Item = styled.div`
   &:hover {
     transform: scale(1.02);
   }
+
+  @media(max-width: 450px) {
+    width: 48%;
+    height: 2rem;
+    margin-bottom: .5rem;
+  }
 `
 
 export const Symbol = styled.div`
   border-radius: 5px 0 0 5px;
   width: 2.8rem;
-  height: calc(2.5rem - 2px);
+  height: 100%;
   display: flex;
   align-items: center;
   background-color: #320d68;
@@ -120,14 +140,23 @@ export const Symbol = styled.div`
   justify-content: center;
   font-weight: bold;
   color: #c2c4c7;
+
+  @media(max-width: 450px) {
+    font-size: 1rem;  
+    width: 2.4rem;
+  }
 `;
 
 export const Name = styled.span`
-  width: 8.7rem;
+  width: 72%;
   font-size: 1rem;
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
+
+  @media(max-width: 450px) {
+    font-size: .9rem;  
+  }
 `;
 
 
