f293539c521dfef81ef4c2eaee4e02360f634193
M4rcusVinicius <marcus.vinicius.mvap@gmail.com>
2021-05-24 15:19:16 -0300

    GA event

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,9 +1,19 @@
 import React from "react"
+import ReactGA from 'react-ga';
 import Image from 'next/image'
+
 import * as S from "./styled"
 import * as I from "@styled-icons/boxicons-logos/"
 
-const Footer = () => (
+const Footer = () => {
+  const openMedia = (media) => {
+    ReactGA.event({
+      category: 'Network',
+      action: `The user clicked on the social network: ${media}`
+    });
+  }
+  
+  return (
   <S.Wrapper>
     <S.Profile>
       <S.Image>
@@ -17,16 +27,16 @@ const Footer = () => (
       <S.ProfileInfo>
         <S.Name>Desenvolvido por Marcus Vinícius</S.Name>
         <S.Social>
-          <S.Icon href='https://twitter.com/m4rcus_vin1cius' target="_blank" >
+          <S.Icon onClick={() => openMedia('Twitter')} href='https://twitter.com/m4rcus_vin1cius' target="_blank" >
             <I.Twitter />
           </S.Icon>
-          <S.Icon href='https://discord.com/users/436946870787637248' target="_blank">
+          <S.Icon onClick={() => openMedia('Discord')} href='https://discord.com/users/436946870787637248' target="_blank">
             <I.Discord />
           </S.Icon>
-          <S.Icon href='https://github.com/M4rcusVinicius' target="_blank">
+          <S.Icon onClick={() => openMedia('Github')} href='https://github.com/M4rcusVinicius' target="_blank">
             <I.Github />
           </S.Icon>
-          <S.Icon href='https://www.linkedin.com/in/marcus-vinicius-almeida-pires-b194691b6/' target="_blank">
+          <S.Icon onClick={() => openMedia('Linkedin')} href='https://www.linkedin.com/in/marcus-vinicius-almeida-pires-b194691b6/' target="_blank">
             <I.Linkedin />
           </S.Icon>
         </S.Social>
@@ -34,6 +44,6 @@ const Footer = () => (
     </S.Profile>
     <S.Report href='https://github.com/M4rcusVinicius/Chemistry/issues/new' target="_blank">Reportar bug</S.Report>
   </S.Wrapper>
-)
+)}
 
 export default Footer
